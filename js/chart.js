b2921285c75addb14a212beea6977e289c18bb34
teodortatu <teodortatu@users.noreply.github.com>
2023-05-26 02:10:29 +0300

    Prima varianta

---
+++ js/chart.js
@@ -0,0 +1,48 @@
+                          var xyValues = [
+                            {x:60, y:8},
+                            {x:70, y:8},
+                            {x:80, y:9},
+                            {x:90, y:9},
+                            {x:100, y:9},
+                            {x:110, y:10},
+                            {x:120, y:11},
+                            {x:130, y:14},
+                            {x:140, y:14},
+                          ];
+                          
+                          new Chart("ecg", {
+                            type: "scatter",
+                            data: {
+                              datasets: [{
+                                pointRadius: 4,
+                                pointBackgroundColor: "rgb(0,0,255)",
+                                data: xyValues
+                              }]
+                            },
+                            options: {
+                              legend: {display: false},
+                              scales: {
+                                xAxes: [{ticks: {min: 40, max:160}}],
+                                yAxes: [{ticks: {min: 6, max:16}}],
+                              }
+                            }
+                            
+                          });
+                          new Chart("puls", {
+                            type: "scatter",
+                            data: {
+                              datasets: [{
+                                pointRadius: 4,
+                                pointBackgroundColor: "rgb(0,0,255)",
+                                data: xyValues
+                              }]
+                            },
+                            options: {
+                              legend: {display: false},
+                              scales: {
+                                xAxes: [{ticks: {min: 40, max:160}}],
+                                yAxes: [{ticks: {min: 6, max:16}}],
+                              }
+                            }
+                            
+                          });
